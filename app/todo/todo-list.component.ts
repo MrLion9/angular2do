75d0411622480b33cf1005831f81f685f75c76ef
WorkFin <finthehuman9@gmail.com>
2016-08-11 14:46:47 +0300

    add deleted function

--- app/todo/todo-list.component.ts
+++ app/todo/todo-list.component.ts
@@ -1,12 +1,23 @@
-import {Component} from '@angular/core';
+import {Component, Input, Output} from '@angular/core';
+import {TodoItemComponent} from './todo-item.component';
+import {Todo} from "./todo";
 
 @Component({
     selector: "todo-list",
     templateUrl: "./app/todo/todo-list.component.html",
     styleUrls: ['./app/todo/todo-list.component.css'],
-    inputs: ['todos']
+    directives: [TodoItemComponent]
 })
 
 export class TodoListComponent{
+    @Input() todos: Todo[];
 
+    onTodoDeleted(todo: Todo){
+        if(todo){
+            let index = this.todos.indexOf(todo);
+            if(index > -1){
+                this.todos.splice(index, 1);
+            }
+        }
+    }
 }
